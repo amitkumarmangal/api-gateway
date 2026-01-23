c6b8e6ded4cfc186da5abf15e97c2f56868ded2f
Amit Mangal <amit.mangal01@nagarro.com>
2026-01-23 16:07:41 +0530

    Configuration Details Updated

--- src/main/java/com/business/filter/JwtFilter.java
+++ src/main/java/com/business/filter/JwtFilter.java
@@ -18,12 +18,12 @@ import java.util.*;
 @Component
 public class JwtFilter implements WebFilter {
     private static final String SECRET = "w9pZ4m7Q1z2G5BzYFzL3KQ5XrC9a1N8ZrKJtYv9WlqE=";
-    private static final List<String> BYPASS_URL= List.of("/actuator","/h2-console");
+    private static final List<String> BYPASS_URL= List.of("/actuator","/h2-console","/favicon.ico");
     private static final List<String> ALLOWED_USERS = List.of("admin", "bankuser");
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        if(BYPASS_URL.stream().noneMatch(url -> exchange.getRequest().getURI().getPath().startsWith(url))) {
+        if(BYPASS_URL.stream().noneMatch(url -> exchange.getRequest().getURI().getPath().contains(url))) {
             try {
                 Key key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
                 String auth = exchange.getRequest()
